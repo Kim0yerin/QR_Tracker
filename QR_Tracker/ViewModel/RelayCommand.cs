8f3a9c25daf7866658f1ea371bdce48e2be3d1b3
YerinKim <yr.kim@protnc.co.kr>
2025-07-08 13:28:54 +0900

    feat: RelayCommand 추가

---
+++ QR_Tracker/ViewModel/RelayCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QRtracker.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        // Execute를 실행할 수 있는 상태 확인 이벤트
+        public event EventHandler CanExecuteChanged;
+
+        // Execute를 실행할 수 있는 상태 확인 
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
