0bbd7f44fddb79167e9c7ec933abd0dbc2f30050
YerinKim <yr.kim@protnc.co.kr>
2025-07-08 16:10:15 +0900

    refactor : MainView에 상단바, 메뉴바 구조 도입 및 UserControl 분리
    기존 QrDetectView를 UserControl로 리팩토링하고, MainView에 상단바, 메뉴바 영역과 콘텐츠 영역을 분리함.
    DataTemplate을 활용한 ViewModel-View 매핑도 구성.

---
+++ QR_Tracker/ViewModel/MainViewModel.cs
@@ -0,0 +1,45 @@
+﻿using QRtracker.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QR_Tracker.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ICommand ShowQrCreateCommand { get; }
+
+        public ICommand ShowQrDetectCommand { get; }
+        public ICommand ShowReportCommand { get; }
+
+        private object _currentView;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public object CurrentView
+            {
+                get => _currentView;
+                set { _currentView = value; OnPropertyChanged(); }
+            }
+
+
+        public MainViewModel()
+            {
+                ShowQrDetectCommand = new RelayCommand(_ => CurrentView = new QrDetectViewModel());
+                ShowReportCommand = new RelayCommand(_ => CurrentView = new ReportViewModel());
+
+                // 초기 화면
+                CurrentView = new QrDetectViewModel();
+            }
+
+    }
+}
