32bfabd216b917c7ea6c72e4ec7df38abd14b816
YerinKim <yr.kim@protnc.co.kr>
2025-07-08 12:02:37 +0900

    초기 커밋

--- QR_Tracker/View/QrDetectView.xaml.cs
+++ QR_Tracker/View/QrDetectView.xaml.cs
@@ -18,9 +18,9 @@ namespace QR_Tracker
     /// <summary>
     /// MainWindow.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class QrDetectView : Window
     {
-        public MainWindow()
+        public QrDetectView()
         {
             InitializeComponent();
         }
