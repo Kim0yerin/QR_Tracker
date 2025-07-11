f54b11b011fa20152c0072c7e007f2f2dd3a40fa
YerinKim <yr.kim@protnc.co.kr>
2025-07-11 09:26:13 +0900

    feat : admin 모델 생성 chore : 다국어 지원 영어 수정
    - 관리자 모드 로그인을 위한 모델 생성

---
+++ QR_Tracker/Model/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace QR_Tracker.Model
+{
+    public class Admin
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string AdminId { get; set; }
+        public string Password { get; set; }
+    }
+}
